6a6632a8140ab76ee20f8f1a9dcf616a020438f2
Chirag Varshney <55586574+ChiragV21@users.noreply.github.com>
2021-07-03 17:57:53 +0530

    Add files via upload

---
+++ Sdata.jsx
@@ -0,0 +1,40 @@
+const Sdata = [
+    {
+        imgsrc: "https://occ-0-2590-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVGKObKkoAz0pPEbc671dbR6cBsCiqne9bRmMTr370Rs8Cmrblu3OYxFcQDiYNMgLaeXrWLXRIodbI1kJicnsGtOoTd6dxlc6wzdivW5EC6_1nLESlFe5R_s3Vi0Hw.jpg?r=da1",
+        title: "A Netflix Original Series",
+        sname: "THE SINNER",
+        link: "https://www.netflix.com/in/title/80175802",
+    },
+    {
+        imgsrc: "https://occ-0-2590-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQgNM1AdFMpNOfuGU_0JePH4Je6PiUcAdL443MEOesWzKmomcn4LokPY9npUpqjem6N31n3C1nQx_t3U4ieDCpw0sY2UUiv18xCaGj3zVOW3kkaBOOMqA-_rK9LnRA.jpg?r=302",
+        title: "A Netflix Original Series",
+        sname: "TO THE LAKE",
+        link: "https://www.netflix.com/in/title/81302258",
+    },
+    {
+        imgsrc: "https://occ-0-2590-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABfQ1TnsotTk9IBJ0RynXCPs6b-Or-cTJCgvoNJ7qjY0YJGRy8cfDe3iQh1gbIuyHDRAKpYPvzE1hic6bTMajjia25uLpxeyCp1qgN4txab_5sFfcI6BrQeiu3OaXIQ.jpg?r=af3",
+        title: "A Netflix Original Series",
+        sname: "SENSE 8",
+        link: "https://www.netflix.com/in/title/80025744",
+    },
+    {
+        imgsrc: "https://occ-0-2590-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABSugQMsDmxXdNOtVectXr9pviTTbw9cStPcrJz_Cz_cRibyDSRn0qEjfoT6K2qplxYlJ9hUHlgu45RMPWOcuSVZ_jFLYC5RMS4jhVT4U-q7k15y4EondkdVwRh_b.jpg?r=3ed",
+        title: "A Netflix Original Series",
+        sname: "SACRED GAMES",
+        link: "https://www.netflix.com/in/title/80115328",
+    },
+    {
+        imgsrc: "https://occ-0-2590-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABXj8ff4qbnJKoTbMdvSCxzDop-wzspybcPfeAujKGcxcy_aTTX97n0NIyUKaA-YpmYQbQ9eyh5a__SkAZptGBaJkXBGlo3Bz8rHT6-_S2s5RbSNvMzkbTl4-MFN7.jpg?r=c55",
+        title: "A Netflix Original Series",
+        sname: "MIS MATCHED",
+        link: "https://www.netflix.com/in/title/80220079",
+    },
+    {
+        imgsrc: "https://occ-0-2590-2186.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcxyOCbeRJqpUGpYcE7afBfLxePkjFfb9yNaG4bdONKlqOpQ3i1ZdQhJyGsGvoDJAtt16n2pmNmOTDvAk_J_1l0dXPXc.jpg?r=3a1",
+        title: "A Netflix Original Series",
+        sname: "ZOO",
+        link: "https://www.netflix.com/in/title/80011206",
+    },
+];
+
+export default Sdata;
